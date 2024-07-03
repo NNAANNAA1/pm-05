09f805cd7a6b137f9c73bc1f0923442c39e9d7fe
NNAANNAA1 <157097850+NNAANNAA1@users.noreply.github.com>
2024-07-03 10:39:50 +0700

    Create lk.php

---
+++ lk.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Личный кабинет</title>
+    <link rel="stylesheet" href="style1.css">
+    
+</head>
+<body>
+    <div class="container">
+        <div class="profile-info">
+            <img src="image.png" alt="Аватар" class="avatar">
+            <div class="user-details">
+                <h2>Пётр Петровчич Петров</h2>
+                <p>Телефон: +7 666 666 66 66</p>
+                <p>Почта: pety@mail.ru</p>
+            </div>
+            <button class="btn">Редактировать</button>
+        </div>
+
+        <div class="content">
+            <h3>Мой влог</h3>
+            <p>Я — человек, который ценит баланс в жизни. Работу воспринимаю как возможность развиваться и расти, но не забываю о важности личного времени и увлечений. </p>
+            <p>Фотография - это моя страсть, способ запечатлеть моменты и делиться ими с близкими. Люблю ловить свет, экспериментировать с композицией и передавать эмоции через объектив. </p>
+            <p></p>
+            <div class="container1">
+        <div class="photo">
+            <img src="photo1.jpg" alt="Фото 1">
+        </div>
+        <div class="photo">
+            <img src="photo2.jpg" alt="Фото 2">
+        </div>
+        <div class="photo">
+            <img src="photo3.jpg" alt="Фото 3">
+        </div>
+        <div class="photo">
+            <img src="photo4.jpg" alt="Фото 4">
+        </div>
+      
+    </div>
+    <button class="btn">Редактировать инфонрмацию</button>
+        </div>
+    </div>
+</body>
+</html>
